1341af160d7806cd4fe2fec536409c54f1fbf2de
Marc Hein <info@marc-hein.de>
2018-11-24 23:00:34 +0100

    Initial Commit

---
+++ myQRgenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myQRgenerator
+//
+//  Created by Marc Hein on 24.11.18.
+//  Copyright © 2018 Marc Hein Webdesign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
